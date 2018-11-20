90bdf96d35b6a148ecf67afd9ccc3560dea74a19
gary917 <gary991177@gmail.com>
2018-11-20 15:36:52 +1100

    adding javascript into html

--- application.py
+++ application.py
@@ -1,39 +1,28 @@
 from flask import Flask, render_template, request
 import os
 import config
-import csv
-
-
+import boto3
+import random
 
 HOST_NAME = 's3-ap-southeast-2.amazonaws.com/'
-BUCKET_NAME = 'meme-generator-source' # replace with your bucket name
-
+BUCKET_NAME = 'meme-generator-source'  # replace with your bucket name
+URI = 'https://' + HOST_NAME + BUCKET_NAME
 
 application = Flask(__name__)
 
-#Loads access and secret keys from local rootkey_file
-IAM_PATH = config.FILE_PATH
-rootkey_file = csv.reader(open(IAM_PATH + 'rootkey.csv'))
-keys = list(rootkey_file)
-accss_keys = keys[0][0].split("=")[1]
-secret_keys = keys[1][0].split("=")[1]
+# low level S3 client to find out number of objects in a bucket
+s3client = boto3.client('s3')
+buckets = s3client.list_objects(Bucket=BUCKET_NAME)
+num_object = len(buckets['Contents'])
 
 
 @application.route('/')
 def index():
-
-	return render_template('index.html')
+    random_num = random.randint(0, num_object - 1)
+    obj_key = buckets['Contents'][random_num]['Key']
+    object_URI = URI + '/' + obj_key
+    return render_template('index.html', object_link=object_URI)
 
 
 if __name__ == "__main__":
-	application.run(host = '0.0.0.0', threaded=True)
-
-
-def generateAuthorization(access, secret): #construct Authrorization required for REST requests 
-	#format = Authorization : AWS AWSAccessKeyId:Signature
-	Signature = Base64(HMAC-SHA1(secret, UTF-8-Encoding-Of( StringToSign ) ) );
-	Authorization = "AWS" + " " + access + ":" + Signature;
-
-
-
-
+    application.run(host='0.0.0.0', threaded=True)
