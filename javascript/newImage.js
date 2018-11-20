ced130ad0acc6b548b37e977f22d374169ee039d
gary917 <gary991177@gmail.com>
2018-11-20 15:43:48 +1100

    sadfsdf

---
+++ javascript/newImage.js
@@ -0,0 +1,6 @@
+//javascript to retrieve new image when button is pressed
+
+function newImage(){
+	document.getElementById("meme").src = "https://s3-ap-southeast-2.amazonaws.com/meme-generator-source/vny0sl59ddy11.jpg" 
+}
+
